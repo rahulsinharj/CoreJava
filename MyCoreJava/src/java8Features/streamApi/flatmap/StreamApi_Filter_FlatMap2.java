43f5c31ba6a09255127d05ff9e1db74f0d046267
rahul <rahul@192.168.1.6>
2022-05-19 01:33:08 +0530

    items

--- MyCoreJava/src/java8Features/streamApi/flatmap/StreamApi_Filter_FlatMap2.java
+++ MyCoreJava/src/java8Features/streamApi/flatmap/StreamApi_Filter_FlatMap2.java
@@ -6,6 +6,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamApi_Filter_FlatMap2 {
 
@@ -18,12 +19,29 @@ public class StreamApi_Filter_FlatMap2 {
 		crList.add(new Cricketer(1004, "Vikash", Arrays.asList("belt", "helmet", "ball")));
 
 		// Print the items (remove duplicates) from all the Cricketers ::
-		Set<String> collectAllListItems = crList.stream()							// Convert the List into Stream
+		Set<String> collectAllSetItems = crList.stream()							// Convert the List into Stream
 												.map(cr -> cr.items)				// Map transforms one obj into another form, here each "Cricketer obj" is transformed into Cricketer.items   
-												.flatMap(Collection::stream)		// Convert the "Stream <List<String>>" into "Stream <String>" ; since Cricketer.items is List<String> not String.   
+												.flatMap(Collection::stream)		// Convert the "Stream of <List<String>>" into "Stream of <String>" ; since Cricketer.items is List<String> not String.   
 												.collect(Collectors.toSet());		// Collecting the stream into SET(for removing duplicate items) 	// We can also use Collectors.toList() - duplicate items will remain here.       
 		
-		System.out.println(collectAllListItems);
+		System.out.println(collectAllSetItems);		// Order of ITEMS is not preserved , since we have collected into SET.
+		
+		
+		crList.stream()
+		  .map(cr -> cr.items)				
+		  .flatMap(Collection::stream)	
+		  .distinct()									// Returning "Stream<String>"
+		  .forEach(x -> System.out.print(x +" "));		// Order of ITEMS is preserved here. // Directly printing through forEach method
+
+		System.out.println();
+		
+		List<String> collectList = crList.stream()
+      		  .map(cr -> cr.items)				
+      		  .flatMap(items -> items.stream())	
+      		  .distinct()								// Returning "Stream<String>"
+      		  .collect(Collectors.toList());			// Order of ITEMS is preserved here. // and Storing that List
+
+		System.out.println(collectList);
 		
 	}
 }

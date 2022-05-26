a419403cb0eaf0611277e1443fc9573c6661caf2
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-05-26 23:50:10 +0530

    Cloneable

---
+++ MyCoreJava/src/coreConceptsJava/ObjectCloning.java
@@ -0,0 +1,41 @@
+package coreConceptsJava;
+
+class Boy implements Cloneable {
+	int pen;
+	int pencil;
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
+	}
+
+	@Override
+	public String toString() {
+		return "Boy [pen= " + pen + ", pencil= " + pencil + "]";
+	}
+	
+}
+
+public class ObjectCloning {
+
+	public static void main(String[] args) {
+
+		Boy obj1 = new Boy();
+		obj1.pen = 5;
+		obj1.pencil = 8;
+		
+		System.out.println(obj1);
+		
+		Boy obj2 = null;
+		try 
+		{
+			obj2 = (Boy)obj1.clone();
+		} 
+		catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		System.out.println(obj2); 		// 5  8
+
+	}
+
+}

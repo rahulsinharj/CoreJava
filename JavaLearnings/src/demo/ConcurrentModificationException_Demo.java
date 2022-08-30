5273efa13106946dfe940dfcc75981712dbac582
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-08-30 18:30:50 +0530

    ConcurrentModificationException_Demo

--- JavaLearnings/src/demo/ConcurrentModificationException_Demo.java
+++ JavaLearnings/src/demo/ConcurrentModificationException_Demo.java
@@ -1,34 +1,51 @@
 package demo;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 public class ConcurrentModificationException_Demo {
 
 	public static void main(String[] args) {
 
-		ArrayList<String> list = new ArrayList<>();
+		ArrayList<String> chList = new ArrayList<>(Arrays.asList("a", "b", "c", "d", "e"));
 
-		list.add("b");
-		list.add("a");
-		list.add("c");
-		list.add("d");
-		list.add("e");
-
-//	for(int i=0; i< list.size(); i++)
-//	{
-//		String ch = list.get(i);
-//		if(ch.equals("a"))
-//			list.remove(ch);
-//		
-//		System.out.println(list.size());
-//	}
-
-		for (String ch : list) {
+		for (int i = 0; i < chList.size(); i++) {
+			String ch = chList.get(i);
 			if (ch.equals("a"))
-				list.remove(ch);
+				chList.remove(ch);
+
+			System.out.println(chList.size());
 		}
 
-		System.out.println(list);
+//		for (String ch : chList) {
+//			if (ch.equals("a"))
+//				list.remove(ch);
+//		}
 
+		System.out.println(chList);
+
+		System.out.println("=============================================");
+		
+		List<String> sList = new ArrayList<>(Arrays.asList("vikas", "shivam", "rahul", "gaurav"));
+
+		System.out.println(sList);
+
+		Iterator<String> itr = sList.iterator();
+		while(itr.hasNext())
+		{
+			String s = itr.next();
+			
+			System.out.println(s);
+			if(s.equals("vikas"))
+			{
+//				sList.remove(s);				// Not to use List's remove() method 
+				itr.remove();					// We should use Iterator remove() method to remove this "ConcurrentModificationException"			
+			}
+		}
+
+		System.out.println(sList);
+		
 	}
 }

ea5d6a70bcdb77671a607bbfce7d0c549f38440b
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-10-09 01:08:19 +0530

    StringBuffer is still mutable

--- MyCoreJava/src/coreJava/StringImmutability_Demo.java
+++ MyCoreJava/src/coreJava/StringImmutability_Demo.java
@@ -14,6 +14,13 @@ public class StringImmutability_Demo {
 		System.out.println(sb2);
 		System.out.println("sb1==sb2 : "+(sb1==sb2));
 		
+		
+		final StringBuffer sb = new StringBuffer("sneha");
+//		sb = sb.append("sinha");			// since "StringBuffer sb" is final, that means we cannot reassign any other value to this "sb" again.
+		sb.append("sinha");					// But final doesn't mean that it is also immutable now, NO ; "StringBuffer sb" is still mutable, since we are able to change its state/value. That means making object as 'final' won't make it mutable as well.	 
+		System.out.println(sb);
+		
+		
 	//----------------------------------------------------------------------------
 	System.out.println("----------------------------------------------------------");
 		
@@ -23,7 +30,7 @@ public class StringImmutability_Demo {
 		System.out.println("s1==s2 : "+(s1==s2));
 		
 		s2 = s2.concat("sinha");			// At this line, a new object "rahulsinha" will get create in StringConstatPool, and now s2 will start pointing to this new obj, and s2 will now stop pointing to original "rahul" obj.   
-		
+											//  because String is immutable (original value can't be modified/changed ; if we do so then only a new object with the updated value will be created)
 		System.out.println(s1);
 		System.out.println(s2);
 		System.out.println("s1==s2 : "+(s1==s2));

12958739da0d9ca672f9ff53506f43b6a4a5dce7
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2024-11-27 20:19:28 +0530

    test

--- JavaLearnings/src/demo/MiniCalculator.java
+++ JavaLearnings/src/demo/MiniCalculator.java
@@ -6,32 +6,36 @@ public class MiniCalculator {
 
 	public static void main(String[] args) {
 		Scanner kb = new Scanner(System.in);
-		
+
 		int a = kb.nextInt();
 		char op = kb.next().charAt(0);
 		int b = kb.nextInt();
-		
+
 		System.out.println(cal(a, b, op));
 
 	}
 
 	public static int cal(int a, int b, char op) {
-		
-		switch(op)
-		{
-			case '+' : return a+b;
-						
-			case '-' : return a-b;
-						
-			case '*' : return a*b;
-			
-			case '/' : return a/b;
-						
-			case '%' : return a%b;
-			
+	
+		switch (op) {
+		case '+':
+			return a + b;
+
+		case '-':
+			return a - b;
+
+		case '*':
+			return a * b;
+
+		case '/':
+			return a / b;
+
+		case '%':
+			return a % b;
+
 		}
 		return 0;
-		
+
 	}
 
 }
